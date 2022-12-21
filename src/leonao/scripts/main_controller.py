04c566f49dca320553784b4b352edcc3350659c8
szerpadiaz <szerpadiaz@gmail.com>
2022-12-22 00:22:20 +0100

    Added move_service

--- src/leonao/scripts/main_controller.py
+++ src/leonao/scripts/main_controller.py
@@ -3,16 +3,30 @@
 
 import rospy
 
-def main_controller():
-    rospy.init_node('main_controller', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        rate.sleep()
+class Main_leonao_controller():
+    def __init__(self) -> None:
+        self.record_timer = rospy.Timer(rospy.Duration(0.125), self.record_timer_callback, oneshot=False)
+        self.record_timer.shutdown()
+
+        self.recording_started = False
+        #self.timer_record.run()
+
+    def record_timer_callback(self, timer_event):
+        print("Entered record_timer_callback")
+
+
 
 if __name__ == '__main__':
+
+    rospy.init_node('main_leonao_controller', anonymous=True)
+
     try:
-        main_controller()
+        Main_leonao_controller()
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            hello_str = "hello world %s" % rospy.get_time()
+            rospy.loginfo(hello_str)
+            rate.sleep()
+
     except rospy.ROSInterruptException:
         pass
