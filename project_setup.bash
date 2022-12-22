6682eed0b8ef1089532cb58c376cfd6a90eb7faf
Sergio <sergio.zerpa@tum.de>
2022-12-22 11:12:30 +0100

    Added project_setup with environement variables

---
+++ project_setup.bash
@@ -0,0 +1,6 @@
+# export NAO_IP=10.152.246.59 # orange
+#export NAO_IP=10.152.246.193 # new blue 
+#export NAO_IP=10.152.246.58 # red
+export NAO_IP=10.152.246.108 # new red
+export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/pynaoqi"
+export PYTHONPATH=$HOME/pynaoqi:${PYTHONPATH}
