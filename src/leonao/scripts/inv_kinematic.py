26344f000d8af701a619841c1385844e41be84f5
Sergio <sergio.zerpa@tum.de>
2023-01-19 12:33:34 +0100

    Added a segment to the chain

--- src/leonao/scripts/inv_kinematic.py
+++ src/leonao/scripts/inv_kinematic.py
@@ -3,7 +3,7 @@
 import os 
 from pykdl_utils.kdl_parser import kdl_tree_from_urdf_model
 from urdf_parser_py.urdf import URDF
-from PyKDL import * #Chain, Tree
+import PyKDL
 import rospy
 
 import motion
@@ -11,10 +11,13 @@ import almath
 from naoqi import ALProxy
 import numpy as np
 
+#https://www.orocos.org/book/export/html/800.html
+
+# Xarcro
+
 # path to URDF file
 #path_to_urdf_file = "/opt/ros/kinetic/share/nao_description/urdf/nao_robot_v4_structure.urdf.xacro"
 
-ARM_JOINT_NAMES  = ["RShoulderRoll", "RShoulderPitch", "RElbowYaw", "RElbowRoll", "RWristYaw"] #"RHand"
 
 class InverseKinematics:
     def __init__(self):
@@ -35,21 +38,24 @@ class InverseKinematics:
     def create_chain(self, base_link, end_link):
         chain = self.kdl_tree.getChain(base_link, end_link)
 
-        print("Root link: %s; end link: %s" ,base_link, end_link)
+        print("Root link: " , base_link, "End link:", end_link)
+        
+        self.print_chain(chain)
+
+        return chain
+    
+    def print_chain(self, chain):
         print(chain.getNrOfSegments())
         print(chain.getNrOfJoints())
             
         for i in range(chain.getNrOfSegments()):
-            print(chain.getSegment(i).getJoint())
-            #print(chain.getJoint(i).getName())
+            joint_i = chain.getSegment(i).getJoint()
+            print(joint_i.getName())
 
-        return chain
-    
     def get_end_link_position(self, chain, joint_angles):
-        assert len(joint_angles) >= chain.getNrOfJoints()
-
         # set the values of the joints
         pykdl_joint_array = PyKDL.JntArray(chain.getNrOfJoints())
+
         for i in range(chain.getNrOfJoints()):
             pykdl_joint_array[i] = joint_angles[i]
 
@@ -61,26 +67,30 @@ class InverseKinematics:
         # convert end_effector_pose into a Positon6D array
         x, y, z = end_effector_pose.p
         R = end_effector_pose.M
-        _, _, _, quaternion = np.linalg.svd(R)
-        wx, wy, wz, ww = quaternion
+        wx, wy, wz = R.GetRPY()
+        
         position6d = [x, y, z, wx, wy, wz]
-        print(position6d)
 
         return position6d
 
-    def get_joints_angles(self, chain, position6d, joints_angles_initial_guess):
+    def get_joints_angles(self, chain, position6d):
         
         # Define the desired end-effector position
-        end_effector_position = PyKDL.Vector(position6d[0], position6d[1], position6d[2])
+        end_effector_position = PyKDL.Frame()
+        end_effector_position.p[0] = position6d[0]
+        end_effector_position.p[1] = position6d[1]
+        end_effector_position.p[2] = position6d[2]
+        end_effector_position.M = PyKDL.Rotation.RPY(position6d[3],position6d[4],position6d[5])
 
         # use initial guess from previous value? or all zeros?
+        # joints_angles_initial_guess
         joints_array_initial_guess = PyKDL.JntArray(chain.getNrOfJoints())
 
         # Create an FK solver for the chain
         fk_p_solver = PyKDL.ChainFkSolverPos_recursive(chain)
 
         # Create an IK solver for the chain
-        ik_v_solver = PyKDL.ChainIkSolverVel_pinv(chain) #,ik_solver_init_pos?
+        ik_v_solver = PyKDL.ChainIkSolverVel_pinv(chain)
         ik_p_solver = PyKDL.ChainIkSolverPos_NR(chain, fk_p_solver, ik_v_solver)
 
         # ChainIkSolverPos_NR iksolver1(chain1,fksolver1,iksolver1v,100,1e-6) ?
@@ -88,7 +98,7 @@ class InverseKinematics:
 
         # Compute the inverse kinematics
         joints_array = PyKDL.JntArray(chain.getNrOfJoints())
-        ik_status = ik_p_solver.CartToJnt(joints_array, end_effector_position, joints_array_initial_guess)
+        ik_status = ik_p_solver.CartToJnt(joints_array_initial_guess, end_effector_position, joints_array)
         # ik_p_solver.CartToJnt(q_init,F_dest,q) ?
 
         if  ik_status >= 0:
@@ -98,7 +108,10 @@ class InverseKinematics:
             print("IK solution not found")
 
         #convert joint array to list?
-        joints_angles = joints_array
+        joints_angles = []
+        for i in range(chain.getNrOfJoints()):
+            joints_angles.append(joints_array[i])
+
         return joints_angles
             
 
@@ -111,8 +124,7 @@ class Motion_tester():
         self.arm_joints_names = self.motion_proxy.getBodyNames(self.arm_name) # should be equal to ARM_JOINT_NAMES
         self.arm_joints_limits = self.motion_proxy.getLimits(self.arm_name)
         
-        print("arm_joints", self.arm_joints_names) #str(arm_joints)
-        print("ARM_JOINT_NAMES: ", ARM_JOINT_NAMES)
+        print("arm_joints", self.arm_joints_names)
         self.print_joints_limits(self.arm_joints_names, self.arm_joints_limits)
 
     def disable_arm_stiffness(self):
@@ -144,9 +156,16 @@ def main():
 
     ik = InverseKinematics()
     base_link = 'torso'
-    end_link = 'r_wrist'
+    end_link = 'r_gripper'
     arm_chain = ik.create_chain(base_link, end_link)
-    
+
+    joint = PyKDL.Joint('PenTip')
+    frame = PyKDL.Frame(PyKDL.Vector(0.08,0.0,0.035))
+    segment = PyKDL.Segment('PenTip',joint,frame)
+    arm_chain.addSegment(segment)
+
+    ik.print_chain(arm_chain)
+
     motion_tester = Motion_tester()
     measured_position6D = motion_tester.get_arm_end_position()
     measured_joints = motion_tester.get_arm_joints()
@@ -155,6 +174,10 @@ def main():
 
     calculated_position6D = ik.get_end_link_position(arm_chain, measured_joints)
     print("calculated_position6D: ", calculated_position6D)
+
+    new_joints = ik.get_joints_angles(arm_chain, calculated_position6D)
+    new_position6D = ik.get_end_link_position(arm_chain, new_joints)
+    print("new_position6D: ", new_position6D)
 
     while not rospy.is_shutdown():
         # do any other calculations here
