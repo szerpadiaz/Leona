911ff7fc24722ad036726fcdc1075ce3c8c5bc31
Michael Sodamin <michael.sodamin@tum.de>
2023-02-02 16:28:51 +0100

    improvements

--- src/leonao/scripts/main_controller.py
+++ src/leonao/scripts/main_controller.py
@@ -11,16 +11,64 @@ import random
 from picture_taker import *
 from picture_painter import *
 
-INTRO_MSG_1 = ["Hello! I am Leonao, Leonao Davinci. Welcome to my studio!", "Step right up, step right up! I, the great Leonardo Da Vinci, am here to paint your portrait!"]
-INTRO_MSG_2 = ["If you want a beautiful picture, please press the button on the front of my head", "I can draw a masterpiece for you. Just come here and touch my head to start."]
-TAKING_PICTURE_INSTRUCTIONS_1 = ["OK! lets get started", "Alright, let's do this!", "Here we go!"]
-TAKING_PICTURE_INSTRUCTIONS_2 = ["Please position yourself in front of me and get ready for the picture.", "Hold still, my friend! I don't want to capture your nerves, just your beauty."]
 
-MSG_AFTER_SUCCESS_PICTURE_TAKEN = "Well done! please relax and enjoy while I paint you. Don't worry, I'll make you look better than you do in real life."
-MSG_PICTURE_TAKEN_FAILED = "OH no, let's try again!"
-MSG_PAINTING_IS_DONE = "I am done, here it is your beautiful picture! That would be 5 Euro please!"
-MSG_THANKS = "Thanks for letting me sketching you."
-MSG_BEFORE_SIESTA = "I am not going to take a Siesta. Wake me up!"
+
+MSG_THANKS = "Thank you for visiting my studio, come back soon!"
+MSG_BEFORE_SIESTA = "I am going to take a nap now. See you later!"
+
+INTRO_MSG_1 = [
+"Greetings! I am LeoNao Davinci, the mechanical artist.",
+"Welcome to my workshop, where I create beautiful portraits just like Leonardo Da Vinci.",
+"How do you do? I am LeoNao, a robot that paints portraits in the style of Leonardo Da Vinci.",
+"Good day! I am LeoNao, a robot that emulates the famous Leonardo Da Vinci.",
+"Salutations! I am LeoNao Davinci, a robot with the talent of Leonardo Da Vinci.",
+"Step right up, step right up! I, the great Leonardo Da Vinci, am here to paint your portrait!"
+]
+INTRO_MSG_2 = [
+"If you desire a stunning portrait, just press the button on my head",
+"I would be honored to paint your portrait. Just touch my head to start.",
+"Let's create a masterpiece together! Just press the button on my head.",
+"Are you ready for a beautiful portrait? Simply touch the button on my head to begin.",
+"It would be my pleasure to paint your portrait. Please touch the button on my head to start.",
+"I can draw a masterpiece for you. Just come here and touch my head to start."
+]
+TAKING_PICTURE_INSTRUCTIONS_1 = [
+"Perfect! Let's get started.",
+"Excellent! Here we go!",
+"Fantastic! Let's begin.",
+"Great! I'm ready when you are.",
+"Awesome! Let's make a masterpiece."
+]
+TAKING_PICTURE_INSTRUCTIONS_2 = [
+"Please stand in front of me and hold still for the picture.",
+"Get ready for your close-up! Stand in front of me and hold still."
+"Just stand in front of me and stay still for the picture."
+"Position yourself in front of me and hold still. I don't want to capture any movement."
+"Take your place in front of me and hold still. I want to capture your true beauty.",
+"Hold still, my friend! I don't want to capture your nerves, just your beauty."
+]
+MSG_AFTER_SUCCESS_PICTURE_TAKEN = [
+"Wonderful! Now relax and let me paint your portrait.",
+"Marvelous! I'll make sure you look even more stunning in the portrait.",
+"Excellent! Just sit back and enjoy while I create your portrait.",
+"Fantastic! I'll make sure to capture your best features in the portrait.",
+"Superb! I'll bring out your beauty in the portrait.",
+"Well done! please relax and enjoy while I paint you. Don't worry, I'll make you look better than you do in real life."
+]
+MSG_PICTURE_TAKEN_FAILED = [
+"Oh dear, let's try again.",
+"Darn, let's take the picture again.",
+"Too bad, let's try taking the picture again.",
+"Oh no, let's retry the picture.",
+"Bummer, let's take the picture one more time."
+]
+MSG_PAINTING_IS_DONE = [
+"It's finished! Here is your beautiful portrait! That will be 5 Euro, please.",
+"Ta-da! Here is your stunning portrait! That will be 5 Euro, if you please.",
+"Voila! Here is your gorgeous portrait! May I have 5 Euro, please?",
+"Et voila! Here is your amazing portrait! That will be 5 Euro, thank you.",
+"Behold! Here is your breathtaking portrait!"]
+
 
 class Main_leonao_controller():
     def __init__(self):
