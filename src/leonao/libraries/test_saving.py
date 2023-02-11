45d0f009655b448f719747e1246d657618440609
Michael Sodamin <>
2023-02-11 22:25:35 +0100

    commented

--- src/leonao/libraries/test_saving.py
+++ src/leonao/libraries/test_saving.py
@@ -1,3 +1,9 @@
+"""
+A shrot program to simply test triggering the watchdog through a cv2,imwrite 
+We do this, as we were having problems through double triggering of events
+"""
+
+
 import cv2
 
 img = cv2.imread("../watchfolder/sketch_face.jpg")
